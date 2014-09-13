16d0c7d43824872dfb9ffee90891ff6567315d51
awen <awen_eagle@sina.com>
2014-09-13 19:16:07 +0800

    env 2.0

--- entry.php
+++
@@ -1,5 +0,0 @@
-<?php
-include __DIR__ . "/core/env.php";
-$env = new Env(array());
-$env->root = __DIR__;
-$env->safe_request($_SERVER['REQUEST_URI']);
--- services/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-class   Index {
-    public function run(){
-    }
-}
